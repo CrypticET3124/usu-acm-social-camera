a3d3a7cc42f5b592c5b1c82b306366edd5ec7994
Spencer Smith <spencerliamsmith@gmail.com>
2026-02-16 02:08:47 -0700

    sticker translate

--- stickers/view.tsx
+++ stickers/view.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { StickerRegistry } from "./registry";
 import { StickerPack } from "./types";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -26,28 +26,18 @@ export default function StickerView({
 
   const x = useSharedValue(t.x);
   const y = useSharedValue(t.y);
-  const scale = useSharedValue(t.scale);
-  const rotation = useSharedValue(t.rotation);
-
   const startX = useSharedValue(t.x);
   const startY = useSharedValue(t.y);
-  const startScale = useSharedValue(t.scale);
-  const startRotation = useSharedValue(t.rotation);
 
-  // pivot / anchor in local coords
-  const anchorX = useSharedValue(0);
-  const anchorY = useSharedValue(0);
+  const initialScale = t.scale;
+  const initialRotation = t.rotation;
 
   useEffect(() => {
     x.value = t.x;
     y.value = t.y;
-    scale.value = t.scale;
-    rotation.value = t.rotation;
 
     startX.value = t.x;
     startY.value = t.y;
-    startScale.value = t.scale;
-    startRotation.value = t.rotation;
   }, [t.x, t.y, t.scale, t.rotation]);
 
   const { bringToFront, select, setTransform, commit } = registry.actions;
@@ -58,8 +48,8 @@ export default function StickerView({
       {
         x: x.value,
         y: y.value,
-        scale: scale.value,
-        rotation: rotation.value,
+        scale: initialScale,
+        rotation: initialRotation,
       },
       false,
     );
@@ -67,7 +57,7 @@ export default function StickerView({
   };
 
   const pan = Gesture.Pan()
-    .maxPointers(1)
+    .maxPointers(1) // single finger: translate only
     .onBegin(() => {
       scheduleOnRN(bringToFront, stickerId);
       scheduleOnRN(select, stickerId);
@@ -82,62 +72,15 @@ export default function StickerView({
     .onEnd(() => {
       scheduleOnRN(commitToRegistry);
     });
-
-  const pinch = Gesture.Pinch()
-    .onBegin((e) => {
-      scheduleOnRN(bringToFront, stickerId);
-      scheduleOnRN(select, stickerId);
-
-      startScale.value = scale.value;
-
-      anchorX.value = e.focalX - x.value;
-      anchorY.value = e.focalY - y.value;
-    })
-    .onUpdate((e) => {
-      x.value = e.focalX - anchorX.value;
-      y.value = e.focalY - anchorY.value;
-
-      scale.value = startScale.value * e.scale;
-    })
-    .onEnd(() => {
-      scheduleOnRN(commitToRegistry);
-    });
-
-  const rotate = Gesture.Rotation()
-    .onBegin((e) => {
-      scheduleOnRN(bringToFront, stickerId);
-      scheduleOnRN(select, stickerId);
-
-      startRotation.value = rotation.value;
-
-      anchorX.value = e.anchorX - x.value;
-      anchorY.value = e.anchorY - y.value;
-    })
-    .onUpdate((e) => {
-      x.value = e.anchorX - anchorX.value;
-      y.value = e.anchorY - anchorY.value;
-
-      rotation.value = startRotation.value + e.rotation;
-    })
-    .onEnd(() => {
-      scheduleOnRN(commitToRegistry);
-    });
-
-  const twoFinger = Gesture.Simultaneous(pinch, rotate);
-  const gesture = Gesture.Simultaneous(pan, twoFinger);
+  const gesture = pan;
 
   const style = useAnimatedStyle(() => {
     return {
       transform: [
         { translateX: x.value },
         { translateY: y.value },
-
-        { translateX: anchorX.value },
-        { translateY: anchorY.value },
-        { rotate: `${rotation.value}rad` },
-        { scale: scale.value },
-        { translateX: -anchorX.value },
-        { translateY: -anchorY.value },
+        { rotate: `${initialRotation}rad` },
+        { scale: initialScale },
       ],
     };
   });
